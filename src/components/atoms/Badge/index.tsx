1f1c5843aa3880a33f6274a9927a46df183d24f8
Yoshin <taigaby0411@gmail.com>
2022-09-10 23:05:18 +0900

    Badgeコンポーネント作成

---
+++ src/components/atoms/Badge/index.tsx
@@ -0,0 +1,32 @@
+import styled from 'styled-components'
+
+const BadgeWrapper = styled.div<{ backgroundColor: string }>`
+  border-radius: 20px;
+  height: 20px;
+  min-width: 20px;
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  background-color: ${({backgroundColor}) => backgroundColor};
+`
+
+const BadgeText = styled.p`
+  color: white;
+  font-size: 11px;
+  user-select: none;
+`
+
+interface BadgeProps {
+  content: string;
+  backgroundColor: string;
+}
+
+const Badge = ({ content, backgroundColor }: BadgeProps) => {
+  return (
+    <BadgeWrapper backgroundColor={backgroundColor}>
+      <BadgeText>{content}</BadgeText>
+    </BadgeWrapper>
+  )
+}
+
+export default Badge
