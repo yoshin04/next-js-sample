fd92b6eac9cf3a9f3cf07c849369d6b3c85ccf3b
Yoshin <taigaby0411@gmail.com>
2022-09-24 08:45:24 +0900

    SigninPage実装

---
+++ src/containers/SigninFormContainer.tsx
@@ -0,0 +1,31 @@
+import SigninForm from 'components/organisms/SigninForm'
+import { useAuthContext } from 'contexts/AuthContext'
+import { useGlobalSpinnerActionsContext } from 'contexts/GlobalSpinnerContext';
+
+interface SigninFormContainerProps {
+  onSignin: (error?: Error) => void
+}
+
+const SigninFormContainer = ({ onSignin }: SigninFormContainerProps) => {
+  const { signin } = useAuthContext()
+  const setGlobalSpinner = useGlobalSpinnerActionsContext()
+
+  const handleSignin = async (username: string, password: string) => {
+    try {
+      setGlobalSpinner(true)
+      await signin(username, password)
+      onSignin && onSignin()
+    } catch (err: unknown) {
+      if (err instanceof Error) {
+        window.alert(err.message)
+        onSignin && onSignin(err)
+      }
+    } finally {
+      setGlobalSpinner(false)
+    }
+  }
+
+  return <SigninForm onSignin={handleSignin} />
+}
+
+export default SigninFormContainer
